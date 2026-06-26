910efc7b078eeebbe939b0acb85db5cf5d957a11
Craole <32288735+Craole@users.noreply.github.com>
2026-06-26 14:30:59 -0500

    formatters

--- utilities/formatter/default.nix
+++ utilities/formatter/default.nix
@@ -5,20 +5,68 @@
   projectRoot,
   packages,
 }: let
-  evalFor = pkgs:
-    evalModule pkgs {
-      projectRootFile = "flake.nix";
-      programs = {
-        alejandra.enable = true;
-        statix.enable = true;
-      };
-    };
-
-  evaluated = forEachSystem {
-    inherit packages;
-    fn = evalFor;
-  };
-
+  evaluated = import ./config.nix {inherit forEachSystem evalModule packages;};
+  # evaluated = forEachSystem {
+  #   inherit packages;
+  #   fn = pkgs:
+  #     evalModule pkgs {
+  #       projectRootFile = "flake.nix";
+  #       # Global exclusions (generated files, secrets, lock files)
+  #       settings.excludes = [
+  #         "flake.lock"
+  #         "secrets/secrets.yaml"
+  #         ".sops.yaml"
+  #         "secrets/ssh/**"
+  #         "*.min.js"
+  #         "*.min.css"
+  #         "node_modules/*"
+  #         "target/*"
+  #         "result*"
+  #         ".direnv/*"
+  #       ];
+  #       programs = {
+  #         #~@ Nix
+  #         alejandra.enable = true;
+  #         statix.enable = true;
+  #         deadnix.enable = true;
+  #         #~@ Shellscript
+  #         shfmt.enable = true;
+  #         shellcheck.enable = true;
+  #         #~@ Rust
+  #         rustfmt.enable = true;
+  #         leptosfmt.enable = true;
+  #         #~@ Web / Frontend
+  #         biome.enable = true;
+  #         prettier.enable = true;
+  #         #~@ Data
+  #         taplo.enable = true; #? TOML
+  #         yamlfmt.enable = true; #? YAML
+  #         jsonfmt.enable = true; #? JSON
+  #         sql-formatter = {
+  #           enable = true;
+  #           dialect = "sqlite";
+  #         };
+  #         # Documentation
+  #         mdformat = {
+  #           enable = true;
+  #           plugins = ps:
+  #             with ps; [
+  #               mdformat-gfm
+  #               mdformat-footnote
+  #             ];
+  #         };
+  #         typstyle = {
+  #           enable = true;
+  #           lineWidth = 100;
+  #           wrapText = true;
+  #         };
+  #         # GitHub Actions (if you add workflows later)
+  #         actionlint.enable = true;
+  #         # Images (lossless PNG optimization for web assets)
+  #         oxipng.enable = true;
+  #       };
+  #     };
+  # };
   formatter =
     mapAttrs
     (_: eval: eval.config.build.wrapper)
