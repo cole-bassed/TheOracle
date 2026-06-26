ed770ad0c1ca21752d655af8d7389920c2ca94e7
Craole <32288735+Craole@users.noreply.github.com>
2026-06-25 21:53:34 -0500

    libs

--- utilities/formatter/default.nix
+++ utilities/formatter/default.nix
@@ -1,38 +1,19 @@
-{
-  projectRoot,
-  libraries,
-  packages,
-  ...
-}: let
-  inherit (libraries.treefmt) evalModule;
-  inherit (libraries.systems) forEachSystem;
+{lix, ...}: let
+  inherit (lix.treefmt) evalModule projectRoot;
+  inherit (lix.systems) forEachSystem;
 
-  # Pass down the decoupled pkgs mapping right at the computation site
-  evalFor = forEachSystem packages (
-    pkgs:
-      evalModule pkgs {
-        # projectRootFile = "flake.nix";
-        inherit projectRoot;
+  evalFor = pkgs:
+    evalModule pkgs {
+      projectRootFile = "flake.nix";
+      programs = {
+        alejandra.enable = true;
+        statix.enable = true;
+      };
+    };
+in {
+  formatter = forEachSystem (pkgs: (evalFor pkgs).config.build.wrapper);
 
-        programs = {
-          alejandra.enable = true;
-          rustfmt.enable = true;
-          shfmt.enable = true;
-        };
-
-        settings.global.excludes = ["secrets/*" "*.yaml" "*.md"];
-      }
-  );
-
-  formatter =
-    forEachSystem
-    (pkgs: (evalFor pkgs).config.build.wrapper);
-
-  checks =
-    forEachSystem
-    (pkgs: {formatting = (evalFor pkgs).config.build.check projectRoot;});
-  # packages =
-  #   mapAttrs
-  #   (system: eval: {default = eval.config.build.wrapper;})
-  #   evalFor;
-in {inherit formatter checks;}
+  checks = forEachSystem (pkgs: {
+    formatting = (evalFor pkgs).config.build.check projectRoot;
+  });
+}
